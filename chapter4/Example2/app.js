f7226082c1822c22ed365a4292b65c764307650f
io <webworkk@mail.ru>
2017-09-11 16:49:32 +0300

    fix app.js on empty value and keyCode(Enter)

--- chapter4/Example2/app.js
+++ chapter4/Example2/app.js
@@ -2,9 +2,28 @@ var main = function() {
 	'use strict';
 	$(".comment-input button").on("click", function(event) {
 		var $text_comment = $(".comment-input input").val();
+		if ($text_comment == "") {
+			return;
+		}
 		var $new_comment = $("<p>");
 		$new_comment.text($text_comment);
 		$(".comments").append($new_comment);
+		$(".comment-input input").val("");
+	});
+
+	$(".comment-input input").on("keypress", function(event) {
+		if (event.keyCode == 13) {
+			var $text_comment = $(".comment-input input").val();
+			if ($text_comment == "") {
+				return;
+			}
+			var $new_comment = $("<p>");
+			$new_comment.text($text_comment);
+			$(".comments").append($new_comment);
+			$(".comment-input input").val("");
+		}
 	});
 };
+
+
 $(document).ready(main);
