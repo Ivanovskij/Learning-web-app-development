85dca8d7d67bb5046948afcc7eac61aaf5ad3015
io <webworkk@mail.ru>
2017-09-11 16:34:16 +0300

    make up app.js - script add comment from input area

--- chapter4/Example2/app.js
+++ chapter4/Example2/app.js
@@ -1,7 +1,10 @@
 var main = function() {
 	'use strict';
 	$(".comment-input button").on("click", function(event) {
-		$(".comments").append("<p>Это новый ком</p>");
+		var $text_comment = $(".comment-input input").val();
+		var $new_comment = $("<p>");
+		$new_comment.text($text_comment);
+		$(".comments").append($new_comment);
 	});
 };
 $(document).ready(main);
