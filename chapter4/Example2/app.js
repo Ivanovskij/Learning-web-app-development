5cc9801d900134285fd973b88c81e63d4e684fa8
io <webworkk@mail.ru>
2017-09-11 19:29:26 +0300

    fix app.js - refactor code, add animtion

--- chapter4/Example2/app.js
+++ chapter4/Example2/app.js
@@ -1,26 +1,25 @@
 var main = function() {
 	'use strict';
-	$(".comment-input button").on("click", function(event) {
+	var addCommentFromInputBox = function() {
 		var $text_comment = $(".comment-input input").val();
 		if ($text_comment == "") {
 			return;
 		}
 		var $new_comment = $("<p>");
+		$new_comment.hide();
 		$new_comment.text($text_comment);
 		$(".comments").append($new_comment);
+		$new_comment.fadeIn();
 		$(".comment-input input").val("");
+	};
+
+	$(".comment-input button").on("click", function(event) {
+		addCommentFromInputBox();
 	});
 
 	$(".comment-input input").on("keypress", function(event) {
 		if (event.keyCode == 13) {
-			var $text_comment = $(".comment-input input").val();
-			if ($text_comment == "") {
-				return;
-			}
-			var $new_comment = $("<p>");
-			$new_comment.text($text_comment);
-			$(".comments").append($new_comment);
-			$(".comment-input input").val("");
+			addCommentFromInputBox();
 		}
 	});
 };
