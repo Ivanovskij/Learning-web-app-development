dbf3b29f6c1e377640b755f925bfe4a12d32d3b0
io <webworkk@mail.ru>
2017-09-12 13:41:32 +0300

    fix app.js - add dynamicly content

--- chapter4/Amazeriffic/js/app.js
+++ chapter4/Amazeriffic/js/app.js
@@ -1,7 +1,50 @@
-var main = function() {
+var main = function () {
 	"use strict";
 
-	var sizeTabs = 3;
+	var toDos = [
+		"Закончить писать эту книгу",
+		"Вывести Грейси на прогулку в парк",
+		"Ответить на электронные письма",
+		"Подготовиться к лекции в понедельник",
+		"Обновить несколько новых задач",
+		"Купить продукты"
+	];
+
+	$(".tabs a span").toArray().forEach(function (_element) {
+		$(_element).on("click", function() {
+			var $element = $(_element);
+			$(".tabs a span").removeClass("active");
+			$element.addClass("active");
+			$("main .content").empty();
+
+			var $content;
+			if ($element.parent().is(":nth-child(1)")) {
+				$content = $("<ul>");
+				toDos.forEach(function(todo) {
+					$content.append($("<li>").text(todo));
+				});
+				$("main .content").append($content);
+			} else if ($element.parent().is(":nth-child(2)")) {
+				$content = $("<ul>");
+				toDos.forEach(function(todo) {
+					$content.append($("<li>").text(todo));
+				});
+				$("main .content").append($content);
+			} else if ($element.parent().is(":nth-child(3)")) {
+				
+			}
+
+			return false;
+		});
+		
+	});
+
+	// фальшивый щелчек
+	// Это динамически сконструирует содержимое
+	$(".tabs a:first-child span").trigger("click");
+
+	// Обработка активного таба
+	/*var sizeTabs = 3;
 	for (var tabNumber = 1; tabNumber <= sizeTabs; tabNumber++) {
 		var $tabSelector = $(".tabs a:nth-child(" + tabNumber + ") span");
 		$tabSelector.on("click", function() {
@@ -10,8 +53,8 @@ var main = function() {
 			$("main .content").empty();
 			return false;
 		});
-	}
-	
-}
+	}*/
+
+};
 
 $(document).ready(main);
