2e264778a7923ece0506419dc4a24e87e7507033
io <webworkk@mail.ru>
2017-09-12 13:58:38 +0300

    fix app.js - add task dynamicly in toDos

--- chapter4/Amazeriffic/js/app.js
+++ chapter4/Amazeriffic/js/app.js
@@ -12,27 +12,43 @@ var main = function () {
 
 	$(".tabs a span").toArray().forEach(function (_element) {
 		$(_element).on("click", function() {
+			var $content,
+				$input,
+				$button,
+				i;
+
 			var $element = $(_element);
 			$(".tabs a span").removeClass("active");
 			$element.addClass("active");
 			$("main .content").empty();
 
-			var $content;
 			if ($element.parent().is(":nth-child(1)")) {
 				$content = $("<ul>");
-				toDos.forEach(function(todo) {
-					$content.append($("<li>").text(todo));
-				});
-				$("main .content").append($content);
+				for (i = toDos.length - 1; i >= 0; i--) {
+					$content.append($("<li>").text(toDos[i]));
+				}
 			} else if ($element.parent().is(":nth-child(2)")) {
 				$content = $("<ul>");
 				toDos.forEach(function(todo) {
 					$content.append($("<li>").text(todo));
 				});
-				$("main .content").append($content);
 			} else if ($element.parent().is(":nth-child(3)")) {
-				
+				$input = $("<input type='text'>");
+				$button = $("<button>").text("+");
+				$("main .content").append($input).append($button);
+
+				$button.on("click", function() {
+					var $text_input = $("input").val();
+					if ($text_input == "" ||
+						$text_input == null) {
+						return;
+					}
+					toDos.push($text_input);
+					$("input").val("");
+				});
 			}
+
+			$("main .content").append($content);
 
 			return false;
 		});
