bab1f5d3624cfdb20047e750588c7dd4b07541dd
io <webworkk@mail.ru>
2017-09-14 13:46:12 +0300

    fix app.js - make out description by tags name

--- chapter5/Amazeriffic/js/app.js
+++ chapter5/Amazeriffic/js/app.js
@@ -2,7 +2,6 @@ var main = function (toDoObjects) {
 	"use strict";
 
 	var toDos = toDoObjects.map(function (toDo) {
-		alert(toDo.description);
 		return toDo.description;
 	});
 
@@ -23,36 +22,50 @@ var main = function (toDoObjects) {
 				for (i = toDos.length - 1; i >= 0; i--) {
 					$content.append($("<li>").text(toDos[i]));
 				}
+
 			} else if ($element.parent().is(":nth-child(2)")) {
 				$content = $("<ul>");
 				toDos.forEach(function(todo) {
 					$content.append($("<li>").text(todo));
 				});
-			} else if ($element.parent().is(":nth-child(3)")) {
-				var organizedByTag = [ 
-					{
-						"name" : "покупки",
-						"toDos" : ["Купить продукты"]
-					},
-					{
-						"name": "рутина",
-						"toDos": ["Купить продукты", "Вывести Грейси на прогулку в парк "]
-					}
-				];
 
-				
-				organizedByTag.forEach(function (tag) {
-					var $tagName = $("<h3>").text(tag.name);
-					$content = $("<ul>");
+			} else if ($element.parent().is(":nth-child(3)")) {
+				var tags = [];
+
+				toDoObjects.forEach(function (toDo) {
+					toDo.tags.forEach(function (tag) {
+						if (tags.indexOf(tag) === -1) {
+							tags.push(tag);
+						}
+					});
+				});
+
+				var tagObjects = tags.map(function (tag) {
+					var toDosWithTag = [];
+
+					toDoObjects.forEach(function (toDo) {
+						if (toDo.tags.indexOf(tag) !== -1) {
+							toDosWithTag.push(toDo.description);
+						}
+					});
+
+					return { "name": tag, "toDos": toDosWithTag };
+				});
+
+				tagObjects.forEach(function (tag) {
+					var $tagName = $("<h3>").text(tag.name),
+						$content = $("<ul>");
+
 
 					tag.toDos.forEach(function (description) {
 						var $li = $("<li>").text(description);
 						$content.append($li);
 					});
-					
+
 					$("main .content").append($tagName);
 					$("main .content").append($content);
 				});
+
 			} else if ($element.parent().is(":nth-child(4)")) {
 				$input = $("<input type='text'>");
 				$button = $("<button>").text("+");
@@ -78,6 +91,40 @@ var main = function (toDoObjects) {
 	// фальшивый щелчек
 	// Это динамически сконструирует содержимое
 	$(".tabs a:first-child span").trigger("click");
+};
+
+
+var organizeByTags = function(toDoObjects) {
+	// получаем все теги
+	// без дупликатов
+	var tags = [];
+	toDoObjects.forEach(function (todo) {
+		todo.tags.forEach(function (tag) {
+			// убеждаемся, что такого тега еще нет
+			// избегаем дупликтов
+			if (tags.indexOf(tag) === -1) {
+				tags.push(tag);
+			}
+		});
+	});
+
+var tagObjects = tags.map(function (tag) {
+	// здесь мы находим все задачи,
+	// содержащие этот тег
+	var toDosWithTag = [];
+
+	toDoObjects.forEach(function (toDo) {
+		// проверка, что результат
+		// indexOf is *не* равен -1
+		if (toDo.tags.indexOf(tag) !== -1) {
+			toDosWithTag.push(toDo.description);
+		}
+	});
+
+	// мы связываем каждый тег с объектом, который
+	// содержит название тега и массив
+	return { "name": tag, "toDos": toDosWithTag };
+	});
 };
 
 $(document).ready(function() {
