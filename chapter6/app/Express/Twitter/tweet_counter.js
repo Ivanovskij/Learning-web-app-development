766a1a384f2205816bb3bdeabfb7201b4b670929
io <webworkk@mail.ru>
2017-09-18 15:52:46 +0300

    out on console tweets by word and counter

--- chapter6/app/Express/Twitter/tweet_counter.js
+++ chapter6/app/Express/Twitter/tweet_counter.js
@@ -1,10 +1,30 @@
-var ntwitter = requie("ntwitter"),
-	credentials = requie("./credentials.json"),
-	twitter;
+var ntwitter = require("ntwitter"),
+	credentials = require("./credentials.json"),
+	twitter,
+	counts = {};
 
+// set up our twitter objects
 twitter = ntwitter(credentials);
+
+// initialize our counters
+counts.awesome = 0;
+
 // настроим поток twitter с тремя параметрами
 // разделенными запятыми
 twitter.stream(
-	// первый параметр строка
+	'statuses/filter',
+	{ track: ["awesome"] },
+	function(stream) {
+		stream.on('data', function(tweet) {
+			if (tweet.text.indexOf("awesome") > -1) {
+				// increment the awesome counter
+				counts.awesome = counts.awesome + 1;
+			}
+		});
+	}
 );
+
+// counter
+setInterval(function() {
+	console.log("awesome: " + counts.awesome);
+}, 3000);
