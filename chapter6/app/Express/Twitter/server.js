1303ba513c61ed2ab30f973868bfc47cb31d2c70
io <webworkk@mail.ru>
2017-09-18 21:15:11 +0300

    make module tweet_counter.js and run from server.js

---
+++ chapter6/app/Express/Twitter/server.js
@@ -0,0 +1,15 @@
+var express = require("express"),
+	http = require("http"),
+	tweetCounts = require("./tweet_counter.js"),
+	app = express();
+
+// настроим статическую файловую папку
+// для маршрута по умолчанию
+app.use(express.static(__dirname + "/client"));
+
+// создадим HTTP-сервер на базе Express
+http.createServer(app).listen(8080);
+
+app.get("/counts", function(req, res) {
+	res.json(tweetCounts);
+});
