8051b6032fd04e1fb0d6e0e2f7a92e51725421dd
io <webworkk@mail.ru>
2017-09-20 12:06:17 +0300

    get key from redis in server and set value from server

--- chapter7/app/Twitter/tweet_counter.js
+++ chapter7/app/Twitter/tweet_counter.js
@@ -14,21 +14,33 @@ counts.awesome = 0;
 // создание клиента для подключения к Redis
 redisClient = redis.createClient();
 
-// настроим поток twitter с тремя параметрами
-// разделенными запятыми
-twitter.stream(
-	'statuses/filter',
-	{ track: ["awesome"] },
-	function(stream) {
-		stream.on('data', function(tweet) {
-			if (tweet.text.indexOf("awesome") > -1) {
-				// increment the awesome counter
-				redisClient.incr("awesome");
-				counts.awesome = counts.awesome + 1;
-			}
-		});
+// обратный вызов получает два аргумента
+redisClient.get("awesome", function(err, awesomeCount) {
+	if (err !== null) {
+		console.log("ERROR: " + err);
+		return;
 	}
-);
+
+	// установление счетчиков в целое
+	// значение, хранящееся в Redis, или к нулю,
+	// если оно не установлено
+	counts.awesome = parseInt(awesomeCount, 10) || 0;
+
+	twitter.stream(
+		"statuses/filter",
+		{ track: ["awesome"] },
+		function(stream) {
+			stream.on("data", function(tweet) {
+				if (tweet.text.indexOf("awesome") >= -1) {
+					// приращение ключевого значения клиента
+					redisClient.incr("awesome");
+					counts.awesome = counts.awesome + 1;
+				}
+			});
+		}
+	);
+	
+});
 
 // counter
 setInterval(function() {
