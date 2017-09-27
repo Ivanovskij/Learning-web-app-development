e5d68e135d26ee7e7e89d29fe66492c09aed1b19
io <webworkk@mail.ru>
2017-09-27 12:35:52 +0300

    add controllers, fix server.js

--- chapter9/app/Amazeriffic/server.js
+++ chapter9/app/Amazeriffic/server.js
@@ -2,6 +2,7 @@ var express = require("express"),
 	http = require("http"),
 	mongoose = require("mongoose"),
 	ToDo = require("./models/todo.js"),
+	ToDosController = require("./controllers/todos_controller.js"),
 	app = express();
 
 app.use(express.static(__dirname + "/client"));
@@ -12,33 +13,6 @@ mongoose.connect('mongodb://localhost/amazeriffic');
 
 http.createServer(app).listen(8080);
 
-app.get("/todos.json", function(req, res) {
-	ToDo.find({}, function(err, toDos) {
-		res.json(toDos);
-	});
-});
+app.get("/todos.json", ToDosController.index);
 
-app.post("/todos", function(req, res) {
-	console.log(req.body);
-
-	var newToDo = new ToDo( { "description" : req.body.description, 
-				"tags" : req.body.tags } );
-
-	newToDo.save(function (err, response) {
-		if (err != null) {
-			console.log(err);
-			res.send("Error: " + err);
-			return;
-		}
-
-		// клиент ожидает, что будут возвращены все задачи,
-		// поэтому для сохранения совместимости сделаем дополнительный запрос
-		ToDo.find({}, function(err, result) {
-			if (err != null) {
-				// элемент не был сохранен
-				res.send("Error");
-			}
-			res.json(result);
-		});
-	});
-});
+app.post("/todos", ToDosController.create);
